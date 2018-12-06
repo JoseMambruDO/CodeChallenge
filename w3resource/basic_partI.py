8f3b5cae48bfc8f2a85b87cc453bbeae38efbf63
JoseMambruDO <mambrujo@gmail.com>
2018-12-06 12:03:01 -0400

    #19 of #100DaysOfCode

---
+++ w3resource/basic_partI.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+""" Some wonderful lines in python """
+"""Source: https://www.w3resource.com/python-exercises/python-basic-exercises.php"""
+
+from math import *
+
+def q1_twinkle():
+    '''1. Write a Python program to print the following string in a specific
+    format (see the output). Go to the editor Sample String : "Twinkle, twinkle,
+    little star, How I wonder what you are! Up above the world so high, Like a
+     diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+     Output :
+
+    Twinkle, twinkle, little star,
+    	How I wonder what you are!
+    		Up above the world so high,
+    		Like a diamond in the sky.
+    Twinkle, twinkle, little star,
+    	How I wonder what you are
+    '''
+    string = 'Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are'
+
+    #print(string)
+
+    return string
+
+if __name__ == '__main__':
+    print('some test')
---
+++ w3resource/basic_partI_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Some wonderful lines in python """
+"""Source: https://www.w3resource.com/python-exercises/python-basic-exercises.php"""
+
+
+import unittest
+from basic_partI import *
+
+class BasicPartITest(unittest.TestCase):
+
+    def test_q1_twinkle(self):
+        self.assertNotEqual(q1_twinkle(), '')
+        self.assertEqual(q1_twinkle().count('\n'),5)
+        self.assertEqual(q1_twinkle().lower().count('twinkle'),4)
+if __name__ == '__main__':
+    unittest.main()

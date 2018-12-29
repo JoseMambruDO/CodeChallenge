a2b696f3aa339e7afdb7fe3fb1808f00921cdcf4
JoseMambruDO <mambrujo@gmail.com>
2018-12-29 12:54:30 -0400

    43 of #100DaysOfCode

--- w3resource/basic_partI.py
+++ w3resource/basic_partI.py
@@ -444,12 +444,13 @@ def q50_dropNewLineorSpace(value):
     re = value.replace(' ','').replace('\n','')
     return re
 
-
-'''51. Write a Python program to determine profiling of Python programs.
-Note: A profile is a set of statistics that describes how often and for
-how long various parts of the program executed. These statistics can be
-formatted into reports via the pstats module.
-'''
+import cProfile
+def q51_profilePyProgram(expresion):
+    '''51. Write a Python program to determine profiling of Python programs.
+    Note: A profile is a set of statistics that describes how often and for
+    how long various parts of the program executed. These statistics can be
+    formatted into reports via the pstats module.
+    '''
 
 '''52. Write a Python program to print to stderr.'''
 

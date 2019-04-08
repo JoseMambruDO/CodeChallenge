3d15b08af0c810fa35e2acb1fc952321580baff9
JoseMambruDO <mambrujo@gmail.com>
2019-04-07 23:44:08 -0400

    modified:   .gitignore 	new file:   Springboot/worldgbp/.gitignore 	new file:   Springboot/worldgbp/.mvn/wrapper/MavenWrapperDownloader.java 	new file:   Springboot/worldgbp/.mvn/wrapper/maven-wrapper.jar 	new file:   Springboot/worldgbp/.mvn/wrapper/maven-wrapper.properties 	new file:   Springboot/worldgbp/mvnw 	new file:   Springboot/worldgbp/mvnw.cmd 	new file:   Springboot/worldgbp/pom.xml 	new file:   Springboot/worldgbp/src/main/java/com/josemambrudo/worldgbp/WorldgbpApplication.java 	new file:   Springboot/worldgbp/src/main/resources/application.yml 	new file:   Springboot/worldgbp/src/main/resources/db/migration/V1__Initial.sql 	new file:   Springboot/worldgbp/src/test/java/com/josemambrudo/world/DemoApplicationTests.java

---
+++ Springboot/worldgbp/src/main/java/com/josemambrudo/worldgbp/WorldgbpApplication.java
@@ -0,0 +1,13 @@
+package com.josemambrudo.worldgbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldgbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldgbpApplication.class, args);
+	}
+
+}
---
+++ Springboot/worldgbp/src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,2 @@
+
+CREATE TYPE continent AS ENUM ('Asia', 'Africa', 'North America','South America','Antarctica','Europe','Oceania');

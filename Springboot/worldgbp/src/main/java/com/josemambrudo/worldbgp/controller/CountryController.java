1cc53307ba30a41258be9e44f6ade22fb9b721ee
JoseMambruDO <mambrujo@gmail.com>
2019-04-08 23:18:18 -0400

    D2: stackoverflow question 49110818

--- Springboot/worldgbp/src/main/java/com/josemambrudo/worldbgp/controller/CountryController.java
+++ Springboot/worldgbp/src/main/java/com/josemambrudo/worldbgp/controller/CountryController.java
@@ -1,16 +1,13 @@
 package com.josemambrudo.worldbgp.controller;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CountryController {
 
-	@GetMapping("/")
+	@GetMapping("/k")
 	public String main() {
 		return "main";
 	}

49fe4489bb2cdc82e5a6fa9969ede6ec81f53e82
JoseMambruDO <mambrujo@gmail.com>
2019-04-10 22:55:49 -0400

    src/main/java/com/josemambrudo/worldgdp/api/ 	src/main/java/com/josemambrudo/worldgdp/exception/

--- Springboot/worldgdp/src/main/java/com/josemambrudo/worldgdp/crudRepository/CountryRepository.java
+++ Springboot/worldgdp/src/main/java/com/josemambrudo/worldgdp/crudRepository/CountryRepository.java
@@ -1,11 +1,11 @@
 package com.josemambrudo.worldgdp.crudRepository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.josemambrudo.worldgdp.entity.Country;
 
 @Repository
-public interface CountryRepository extends CrudRepository<Country, Long>{
+public interface CountryRepository extends JpaRepository<Country, Long>{
 
 }

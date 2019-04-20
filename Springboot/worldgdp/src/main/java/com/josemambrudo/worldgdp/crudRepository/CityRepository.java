29346595de1a815ab78fba4aa3417a23248b4e18
JoseMambruDO <mambrujo@gmail.com>
2019-04-20 15:35:47 -0400

    D02

--- Springboot/worldgdp/src/main/java/com/josemambrudo/worldgdp/crudRepository/CityRepository.java
+++ Springboot/worldgdp/src/main/java/com/josemambrudo/worldgdp/crudRepository/CityRepository.java
@@ -1,10 +1,17 @@
 package com.josemambrudo.worldgdp.crudRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.josemambrudo.worldgdp.entity.City;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+
+	
+	@Query("SELECT c from City c Where c.name = :name")
+	City getCities(@Param("name") String name);
+
 }
--- Springboot/worldgdp/src/main/java/com/josemambrudo/worldgdp/crudRepository/CountryRepository.java
+++ Springboot/worldgdp/src/main/java/com/josemambrudo/worldgdp/crudRepository/CountryRepository.java
@@ -20,4 +20,15 @@ public interface CountryRepository extends JpaRepository<Country, Long>{
 	
 	@Query("SELECT DISTINCT c.continent from Country c")
 	List<String> getContinents();
+
+	@Query("SELECT c from Country c Where c.code = :code")
+	Country getCountryByCode(@Param("code") String code);
+
+	
+	@Query("SELECT DISTINCT c.headOfState from Country c")
+	List<String> getHeadOfStates();
+
+	@Query("SELECT DISTINCT c.governmentForm from Country c")
+	List<String> getGovernmentTypes();
+
 }

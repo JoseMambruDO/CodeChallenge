a6514affb81aaec14f58453772ac018791329e53
JoseMambruDO <mambrujo@gmail.com>
2019-04-22 23:09:05 -0400

    new file:   PathSplit.java

---
+++ w3resource/java/PathSplit.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class PathSplit{
+
+  public static List splitPathStringToList(String path, String delim){
+      return Arrays.asList( path.split(delim));
+  }
+
+  public static void main(String... arg){
+      String delim = "\\\\";
+      String path = "C:\\Example0\\Example1\\Example2\\Example3\\Example4\\Example5\\";
+      System.out.println(splitPathStringToList(path, delim));
+  }
+
+}

1a857259f6750225e4c4f4dac2185e49382a5563
JoseMambruDO <mambrujo@gmail.com>
2019-04-23 22:13:34 -0400

    Hola

---
+++ Springboot/mydaily/src/test/java/do/josemambru/mydaily/MydailyApplicationTests.java
@@ -0,0 +1,16 @@
+package do.josemambru.mydaily;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MydailyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

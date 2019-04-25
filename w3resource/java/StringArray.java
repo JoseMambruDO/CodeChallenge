473a53340cc3705a5603e5faa4be63dca83cfec4
JoseMambruDO <mambrujo@gmail.com>
2019-04-24 22:57:17 -0400

    new file:   StringArray.java

---
+++ w3resource/java/StringArray.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StringArray{
+
+
+  public static void main(String... args){
+
+    //This routine converts an ArrayList from String to an array of String
+    ArrayList<String> arrayListString = new ArrayList<String> ();
+
+    arrayListString.add("a");
+    arrayListString.add("b");
+    arrayListString.add("c");
+    arrayListString.add("d");
+
+    String[] arrayString =   Arrays.copyOf(arrayListString.toArray(),arrayListString.size(),String[].class);
+
+    System.out.println(arrayString);
+  }
+
+}

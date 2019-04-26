24e9e2cbd1318e9ccabafcfaa29a53705b2f3827
JoseMambruDO <mambrujo@gmail.com>
2019-04-25 23:01:00 -0400

    modified:   PathSplit.java

--- w3resource/java/PathSplit.java
+++ w3resource/java/PathSplit.java
@@ -6,7 +6,8 @@ public class PathSplit{
   public static List splitPathStringToList(String path, String delim){
       return Arrays.asList( path.split(delim));
   }
-
+  
+  //This routine split a string by delimiter and print a array
   public static void main(String... arg){
       String delim = "\\\\";
       String path = "C:\\Example0\\Example1\\Example2\\Example3\\Example4\\Example5\\";

1b9010521185cc723dc907eda8627e597e4a6a7d
JoseMambruDO <mambrujo@gmail.com>
2019-05-15 23:22:35 -0400

    D17 init LiBot

---
+++ LIBot/src/main/java/com/oportunidadTICRD/LIBot/LiBotApplication.java
@@ -0,0 +1,13 @@
+package com.oportunidadTICRD.LIBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiBotApplication.class, args);
+	}
+
+}

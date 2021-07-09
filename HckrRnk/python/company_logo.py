bccf377159b175c4a4b2db626b874193f8de048a
JoseMambruDo <mambrujo@gmail.com>
2021-07-08 22:24:52 -0400

    Hckrrnk D12

---
+++ HckrRnk/python/company_logo.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+import collections
+
+if __name__ == '__main__':
+    s = sorted(input().strip())
+    
+    for t in collections.Counter([e for e in s]).most_common(3):
+        print(t[0],t[1])
+    
+    

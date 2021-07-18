df600350329fab001e2531c43b9ad3e6c97560de
JoseMambruDo <mambrujo@gmail.com>
2021-07-18 13:24:43 -0400

    HckrRnk D21

--- HckrRnk/python/np-min-and-max.py
+++ HckrRnk/python/np-min-and-max.py
@@ -1,3 +1,7 @@
+
+#You are given a 2-D array with dimensions X.
+#Your task is to perform the min function over axis and then find the max of that.
+
 import numpy
 
 n,m = map(int,input().split(' '))

a675a2556a8d60b134eeca0b2226cd13d6b79e36
JoseMambruDo <mambrujo@gmail.com>
2021-07-22 00:01:32 -0400

    HckrRank D23

--- HckrRnk/python/py-set-mutations.py
+++ HckrRnk/python/py-set-mutations.py
@@ -6,7 +6,8 @@ for _ in range(int(input())):
     func, ne = input().split()
     set_e = set(map(int,input().split()))
 
-    getattr(set_a,func,set_e)
-
+    #eval("set_a."+func+'('+str(set_e)+')')
+    
+    getattr(set_a,func)(set_e) # getattr is a cleaner option
+    
 print(sum(list(set_a)))
-

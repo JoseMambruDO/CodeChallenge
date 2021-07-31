5a3835cdde6b40279da20de4583cd615efd15818
JoseMambruDo <mambrujo@gmail.com>
2021-07-30 23:48:51 -0400

    modified:   py-check-subset.py

--- HckrRnk/python/py-check-subset.py
+++ HckrRnk/python/py-check-subset.py
@@ -2,27 +2,27 @@
 # -*- coding: utf-8 -*-
 
 
-#You are given two sets, and .
-#Your job is to find whether set is a subset of set .
+# You are given two sets, and .
+# Your job is to find whether set is a subset of set .
 #
-#If set is subset of set , print True.
-#If set is not a subset of set, print False.
+# If set is subset of set , print True.
+# If set is not a subset of set, print False.
 #
-#Input Format
+# Input Format
 #
-#The first line will contain the number of test cases,T.
-#The first line of each test case contains the number of elements in set .
-#The second line of each test case contains the space separated elements of set .
-#The third line of each test case contains the number of elements in set .
-#The fourth line of each test case contains the space separated elements of set
+# The first line will contain the number of test cases,T.
+# The first line of each test case contains the number of elements in set .
+# The second line of each test case contains the space separated elements of set .
+# The third line of each test case contains the number of elements in set .
+# The fourth line of each test case contains the space separated elements of set
 #
-#.
+# .
 #
-#Constraints
+# Constraints
 #
-#Output Format
+# Output Format
 #
-#Output True or False for each test case on separate lines.
+# Output True or False for each test case on separate lines.
 
 for _ in range(int(input())):
     _, a, _, b = input(), set(input().split()), input(), set(input().split())
